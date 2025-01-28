20960589fc4744f9c2f5b3ab79327969d032bf35
gandak <gandak0930@gmail.com>
2025-01-28 22:38:11 +0800

    fix 8

--- src/app/movies/[movieDetail]/page.tsx
+++ src/app/movies/[movieDetail]/page.tsx
@@ -1,10 +1,11 @@
 import MovieDetailContent from "@/app/_components/movieDetails/MovieDetailContent";
 
 export default function MovieDetailPage({
-  params: { movieDetail },
+  params,
 }: {
   params: { movieDetail: string };
 }) {
+  const { movieDetail } = params;
   return (
     <div className="flex justify-center">
       <MovieDetailContent id={movieDetail} />
