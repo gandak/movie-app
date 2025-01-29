0ce8a6767aee77eb70891373892e1b1e88733276
24LP3548 24LP3548 <24LP3548@G304-CC0228.local>
2025-01-29 09:28:27 +0800

    fuck vercel 11

--- src/app/movies/[movieDetail]/page.tsx
+++ src/app/movies/[movieDetail]/page.tsx
@@ -1,13 +1,13 @@
 import MovieDetailContent from "@/app/_components/movieDetails/MovieDetailContent";
-
-type Params = Promise<{ slug: string[] }>;
+import { MovieId } from "@/util/types";
 
 export default async function MovieDetailPage({
   params,
 }: {
-  params: { movieDetail: string };
+  params: Promise<{ movieDetail: MovieId }>;
 }) {
   const { movieDetail } = await params;
+
   return (
     <div className="flex justify-center">
       <MovieDetailContent id={movieDetail} />
